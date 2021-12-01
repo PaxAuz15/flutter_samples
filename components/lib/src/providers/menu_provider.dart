119bb3822efbc3a0d7a19c397aca72cd817c7339
Luis Auz <luisauz.geek@gmail.com>
2021-11-30 19:23:54 -0500

    feat: Components App: transform string to map

--- components/lib/src/providers/menu_provider.dart
+++ components/lib/src/providers/menu_provider.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/services.dart' show rootBundle;
 
+import 'dart:convert';
+
 class _MenuProvider {
   
   List<dynamic> options = [];
@@ -9,9 +11,10 @@ class _MenuProvider {
   }
 
   void loadData() async {
-    String data = await rootBundle.loadString('data/menu_opts.json');
-    print(data);
-    // return data;
+    final data = await rootBundle.loadString('data/menu_opts.json');
+    Map dataMap = json.decode(data);
+    print(dataMap['rutas']);
+    options = dataMap['rutas'];    // return data;
     // loadData(){
     //   rootBundle.loadString('data/menu_opts.json').then((data) => print(data));
     // }
